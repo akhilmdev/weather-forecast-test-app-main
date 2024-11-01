6ce697f2fdf5426cb3fe73968740fde99869e598
Akhil M <akhilm40@gmail.com>
2024-11-01 11:05:54 +0530

    setting up state and using state value for calling API - added SelectedPlace values to global state - Added typing for selectedPlace - Added @watch decorator in weather-forcast component and triggered API

--- frontend/src/components/weather-forecast.component.ts
+++ frontend/src/components/weather-forecast.component.ts
@@ -1,26 +1,34 @@
-import WeatherService from '@/services/weather-service.service';
-import { Options, Vue } from 'vue-class-component';
-import { Inject } from 'vue-property-decorator';
+import WeatherService from "@/services/weather-service.service";
+import { Options, Vue } from "vue-class-component";
+import { Inject, Watch } from "vue-property-decorator";
+import { mapState, useStore } from "vuex";
 
 @Options({
-  props: {
-  }
+  props: {},
+  computed: mapState({
+    selectedPlace: (state: any) => state.selectedPlace, // Ensure this points to the correct state in Vuex
+  }),
 })
 export default class WeatherForecast extends Vue {
-
-  @Inject('weatherService')
+  @Inject("weatherService")
   public weatherService!: WeatherService;
+  // private selectedPlace = null;
 
-   
+  @Watch("selectedPlace", { immediate: true, deep: true })
+  onSelectedPlaceChange(newValue: any, oldValue: any) {
+    if (oldValue?.lat !== newValue?.lat || oldValue?.lng !== newValue?.lng) {
+      this.weatherService
+        .getWeatherForecast(newValue.lat, newValue.lng)
+        .then((res) => {
+          console.log(res);
+        });
+    }
+    // You can perform additional actions here if needed
+  }
+
   mounted() {
     // TODO - use the latitude and longitude from the search city component
     // TODO - display the weather forecast in the template
     // TODO - Error handling, if the API call fails we should display an error message
-    this.weatherService.getWeatherForecast(52.52, 13.419998).then((res) => {
-      console.log(res);
-    });
   }
-
 }
-
-
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,14 +1,21 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    selectedPlace: null,
   },
   getters: {
+    getSelectedPlace: (state) => state.selectedPlace,
   },
   mutations: {
+    setSelectedPlace(state, place) {
+      state.selectedPlace = place;
+    },
   },
   actions: {
+    updateSelectedPlace({ commit }, place) {
+      commit("setSelectedPlace", place);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
